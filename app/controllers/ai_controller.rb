0ae7e92c452aa4578816927caabc102b57de1c1c
Drew Arrigoni <arrigoni.andrew@gmail.com>
2017-04-30 21:33:46 -0400

    Easy mode

--- app/controllers/ai_controller.rb
+++ app/controllers/ai_controller.rb
@@ -1,11 +1,100 @@
 class AiController < ApplicationController
+  MIN_WIN_LENGTH = 4;
+
   def random
-    binding.pry
-    game_state = params[:game_state]
-    possible_plays = game_state.map.with_index do |column, index|
-      column.any? { |cell| cell == 0 } ? index : nil
-    end.compact.flatten
+    @game_state = params[:game_state]
 
     render json: possible_plays.sample
+  end
+
+  def easy
+    @game_state = params[:game_state]
+    @active_player = params[:active_player].to_i
+
+    winning_column = possible_plays.find do |column|
+      simulated_game_state = play(@active_player, column)
+      result = check_win_for simulated_game_state, @active_player
+      result
+    end
+
+    play_column = winning_column if winning_column
+
+    unless play_column
+      other_player = @active_player == 1 ? 2 : 1
+
+      block_column = possible_plays.find do |column|
+        simulated_game_state = play(other_player, column)
+        result = check_win_for simulated_game_state, other_player
+        result
+      end
+
+      play_column = block_column if block_column
+    end
+
+    play_column = possible_plays.sample unless play_column
+
+    render json: play_column
+  end
+
+  private
+
+  def possible_plays
+    @game_state.map.with_index do |column, index|
+      column.any? { |cell| cell == 0 } ? index : nil
+    end.compact.flatten
+  end
+
+  def duplicate_game_state
+    Marshal.load(Marshal.dump(@game_state)) #.dup is not enough. Results in pointers to sub arrays
+  end
+
+  #Returns a new game state with the play applied
+  def play(player, column)
+    game_state = duplicate_game_state
+    open_index = game_state[column].index(0)
+    if open_index
+      game_state[column][open_index] = player
+      game_state
+    else
+      nil
+    end
+  end
+
+  def check_win_for(game_state, active_player)
+    player_win_string = active_player.to_s * MIN_WIN_LENGTH #CLEVER: "1111"
+
+    column_strings = generate_column_strings(game_state)
+    row_strings = generate_row_strings(game_state)
+    ltr_diagonal_strings = generate_ltr_diagonal_strings(game_state)
+    rtl_diagonal_strings = generate_ltr_diagonal_strings(game_state.reverse)
+
+    all_strings = column_strings + row_strings + ltr_diagonal_strings + rtl_diagonal_strings
+
+    all_strings.any? { |game_string| game_string.include? player_win_string }
+  end
+
+  def generate_column_strings(game_state)
+    game_state.map { |column| column.join }
+  end
+
+  def generate_row_strings(game_state)
+    height = game_state.first.size
+    height.times.map do |row|
+      game_state.map { |col| col[row] }.join
+    end
+  end
+
+  def generate_ltr_diagonal_strings(game_state)
+    diagonal_string_by_index_sum = Hash.new('')
+
+    game_state.each_with_index do |column, column_index|
+      column.each_with_index do |cell_value, row_index|
+        index_sum = column_index + row_index
+
+        diagonal_string_by_index_sum[index_sum] += cell_value.to_s
+      end
+    end
+
+    diagonal_string_by_index_sum.values
   end
 end
--- spec/controllers/ai_controller_spec.rb
+++ spec/controllers/ai_controller_spec.rb
@@ -1,39 +1,196 @@
 require 'rails_helper'
 
 RSpec.describe AiController, type: :controller do
-  describe '#random' do
-    let(:active_player) { 1 }
+  let(:active_player) { 1 }
 
-    subject { response.body.to_i }
+  describe '#random' do
+
+    subject { response.body }
 
     it 'should return a random number representing one of the columns' do
       game_state = [
-        [0, 0, 0 , 0, 0, 0],
-        [0, 0, 0 , 0, 0, 0],
-        [0, 0, 0 , 0, 0, 0],
-        [0, 0, 0 , 0, 0, 0],
-        [0, 0, 0 , 0, 0, 0],
-        [0, 0, 0 , 0, 0, 0],
-        [0, 0, 0 , 0, 0, 0]
-      ]
+        [0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0]
+      ].to_json
+
       put :random, params: { game_state: game_state, active_player: active_player }
 
-      expect(0..6).to include(subject);
+      expect(0..6).to include(subject.to_i);
     end
 
     it 'should not return an invalid column' do
       game_state = [
-        [1, 1, 1 , 1, 1, 0],
-        [1, 1, 1 , 1, 1, 1],
-        [1, 1, 1 , 1, 1, 1],
-        [1, 1, 1 , 1, 1, 1],
-        [1, 1, 1 , 1, 1, 1],
-        [1, 1, 1 , 1, 1, 1],
-        [1, 1, 1 , 1, 1, 1]
-      ]
+        [1, 1, 1, 1, 1, 0],
+        [1, 1, 1, 1, 1, 1],
+        [1, 1, 1, 1, 1, 1],
+        [1, 1, 1, 1, 1, 1],
+        [1, 1, 1, 1, 1, 1],
+        [1, 1, 1, 1, 1, 1],
+        [1, 1, 1, 1, 1, 1]
+      ].to_json
+
       put :random, params: { game_state: game_state, active_player: active_player  }
 
-      expect(subject).to eq 0
+      expect(subject).to eq '0'
+    end
+  end
+
+  describe '#easy' do
+    subject { response.body }
+
+    it 'should play randomly if there is no win or block condition' do
+      game_state = [
+        [0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0]
+      ].to_json
+
+      put :easy, params: { game_state: game_state, active_player: active_player }
+
+      expect(0..6).to include subject.to_i
+    end
+
+    describe 'I WIN plays' do
+      it 'should play a column if it will result in a win for verticals' do
+        game_state = [
+          [0, 0, 0, 0, 0, 0],
+          [1, 1, 1, 0, 0, 0],
+          [0, 0, 0, 0, 0, 0],
+          [0, 0, 0, 0, 0, 0],
+          [0, 0, 0, 0, 0, 0],
+          [0, 0, 0, 0, 0, 0],
+          [0, 0, 0, 0, 0, 0]
+        ].to_json
+
+        put :easy, params: { game_state: game_state, active_player: active_player }
+
+        expect(subject).to eq '1'
+      end
+
+      it 'should play a column if it will result in a win for horizontals' do
+        game_state = [
+          [0, 0, 0, 0, 0, 0],
+          [0, 0, 0, 0, 0, 0],
+          [0, 0, 0, 0, 0, 0],
+          [0, 0, 0, 0, 0, 0],
+          [1, 0, 0, 0, 0, 0],
+          [1, 0, 0, 0, 0, 0],
+          [1, 0, 0, 0, 0, 0]
+        ].to_json
+
+        put :easy, params: { game_state: game_state, active_player: active_player }
+
+        expect(subject).to eq '3'
+      end
+
+      it 'should play a column if it will result in a win for left to right diagonal' do
+        game_state = [
+          [2, 2, 2, 1, 0, 0],
+          [2, 2, 1, 0, 0, 0],
+          [2, 1, 0, 0, 0, 0],
+          [0, 0, 0, 0, 0, 0],
+          [0, 0, 0, 0, 0, 0],
+          [0, 0, 0, 0, 0, 0],
+          [0, 0, 0, 0, 0, 0]
+        ].to_json
+
+        put :easy, params: { game_state: game_state, active_player: active_player }
+
+        expect(subject).to eq '3'
+      end
+
+      it 'should play a column if it will result in a win for right to left diagonal' do
+        game_state = [
+          [0, 0, 0, 0, 0, 0],
+          [0, 0, 0, 0, 0, 0],
+          [0, 0, 0, 0, 0, 0],
+          [0, 0, 0, 0, 0, 0],
+          [2, 1, 0, 0, 0, 0],
+          [2, 2, 1, 0, 0, 0],
+          [2, 2, 2, 1, 0, 0]
+        ].to_json
+
+        put :easy, params: { game_state: game_state, active_player: active_player }
+
+        expect(subject).to eq '3'
+      end
+    end
+
+    describe 'I BLOCK plays' do
+      let(:active_player) { 2 }
+
+      it 'should play a column if it will block a vertical win' do
+        game_state = [
+          [0, 0, 0, 0, 0, 0],
+          [1, 1, 1, 0, 0, 0],
+          [0, 0, 0, 0, 0, 0],
+          [0, 0, 0, 0, 0, 0],
+          [0, 0, 0, 0, 0, 0],
+          [0, 0, 0, 0, 0, 0],
+          [0, 0, 0, 0, 0, 0]
+        ].to_json
+
+        put :easy, params: { game_state: game_state, active_player: active_player }
+
+        expect(subject).to eq '1'
+      end
+
+      it 'should play a column if it will block a horizontal win' do
+        game_state = [
+          [0, 0, 0, 0, 0, 0],
+          [0, 0, 0, 0, 0, 0],
+          [0, 0, 0, 0, 0, 0],
+          [0, 0, 0, 0, 0, 0],
+          [1, 0, 0, 0, 0, 0],
+          [1, 0, 0, 0, 0, 0],
+          [1, 0, 0, 0, 0, 0]
+        ].to_json
+
+        put :easy, params: { game_state: game_state, active_player: active_player }
+
+        expect(subject).to eq '3'
+      end
+
+      it 'should play a column if it will block a win for left to right diagonal' do
+        game_state = [
+          [2, 2, 2, 1, 0, 0],
+          [2, 2, 1, 0, 0, 0],
+          [1, 1, 0, 0, 0, 0],
+          [0, 0, 0, 0, 0, 0],
+          [0, 0, 0, 0, 0, 0],
+          [0, 0, 0, 0, 0, 0],
+          [0, 0, 0, 0, 0, 0]
+        ].to_json
+
+        put :easy, params: { game_state: game_state, active_player: active_player }
+
+        expect(subject).to eq '3'
+      end
+
+      it 'should play a column if it will block a win for right to left diagonal' do
+        game_state = [
+          [0, 0, 0, 0, 0, 0],
+          [0, 0, 0, 0, 0, 0],
+          [0, 0, 0, 0, 0, 0],
+          [0, 0, 0, 0, 0, 0],
+          [1, 1, 0, 0, 0, 0],
+          [2, 2, 1, 0, 0, 0],
+          [2, 2, 2, 1, 0, 0]
+        ].to_json
+
+        put :easy, params: { game_state: game_state, active_player: active_player }
+
+        expect(subject).to eq '3'
+      end
     end
   end
 end
